c1106d022cfd23409bb9ec4cd2f89d3c24b260bf
Koui <hw_1.1@163.com>
2020-01-14 17:11:28 +0800

    Eureka-client setup

---
+++ eureka-service-a/src/main/java/com/koui/springio/cloud/eurekaservicea/EurekaServiceAApplication.java
@@ -0,0 +1,34 @@
+package com.koui.springio.cloud.eurekaservicea;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaServiceAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceAApplication.class, args);
+    }
+
+    @RestController
+    class ServiceAInstance {
+
+        @Autowired
+        private DiscoveryClient discoveryClient;
+
+        @RequestMapping("/serviceA/{applicationName}")
+        public List<ServiceInstance> serviceInstancesByApplicationName (@PathVariable String applicationName) {
+            return this.discoveryClient.getInstances(applicationName);
+        }
+    }
+}
---
+++ eureka-service-a/src/test/java/com/koui/springio/cloud/eurekaservicea/EurekaServiceAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koui.springio.cloud.eurekaservicea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
