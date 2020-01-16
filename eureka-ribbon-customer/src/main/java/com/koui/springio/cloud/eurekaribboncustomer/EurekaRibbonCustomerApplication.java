70a6b0c4d830663bf83fc11e61306134d11f4f6d
Koui <hw_1.1@163.com>
2020-01-16 14:31:33 +0800

    Add Hystrix function

--- eureka-ribbon-customer/src/main/java/com/koui/springio/cloud/eurekaribboncustomer/EurekaRibbonCustomerApplication.java
+++ eureka-ribbon-customer/src/main/java/com/koui/springio/cloud/eurekaribboncustomer/EurekaRibbonCustomerApplication.java
@@ -1,13 +1,16 @@
 package com.koui.springio.cloud.eurekaribboncustomer;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +19,9 @@ import java.util.List;
 
 @SpringBootApplication
 @RestController
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableCircuitBreaker
 public class EurekaRibbonCustomerApplication {
 
     @Bean
@@ -34,10 +39,15 @@ public class EurekaRibbonCustomerApplication {
 
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "reliable")
     public String hi(@RequestParam(value="name", defaultValue = "defaultHI") String name) {
         String greeting = this.restTemplate.getForObject("http://say-hello/greeting", String.class);
 
         return String.format("%s, %s", greeting, name);
+    }
+
+    public String reliable(@RequestParam(value="name", defaultValue = "defaultHI") String name) {
+        return "This is reliable method, currently Hi function can't be used, try again please";
     }
 
     @Autowired
