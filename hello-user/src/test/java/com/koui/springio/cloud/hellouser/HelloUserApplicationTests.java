bfca208dc867bd6a9ea009d5271fae8fa2fbfeb5
Koui <hw_1.1@163.com>
2020-01-16 10:19:02 +0800

    hello user

---
+++ hello-user/src/test/java/com/koui/springio/cloud/hellouser/HelloUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koui.springio.cloud.hellouser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
